c62c701e84c69ea6ff68da294da69f4ce626850a
lichaofeng <lichaofengfcl@163.com>
2018-04-20 18:00:58 +0800

    修改

---
+++ basiclib/src/main/java/com/gofar/component/basiclib/entity/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.gofar.component.basiclib.entity;
+
+/**
+ * @author lcf
+ * @date 2018/4/20 14:03
+ * @since 1.0
+ */
+public class BaseEntity<T> {
+    /**
+     * 请求成功返回码
+     */
+    public static final int SUCCESS_CODE = 0;
+
+    private T data;
+    private int errorCode;
+    private String errorMsg;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public boolean isSuccess() {
+        return errorCode == SUCCESS_CODE;
+    }
+}
