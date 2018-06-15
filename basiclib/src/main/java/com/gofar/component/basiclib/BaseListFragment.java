0d1eaa0533fe06da4b28e165f0fb924bacbcce8e
lichaofeng <lichaofengfcl@163.com>
2018-06-15 18:04:24 +0800

    ListLoader

--- basiclib/src/main/java/com/gofar/component/basiclib/BaseListFragment.java
+++ basiclib/src/main/java/com/gofar/component/basiclib/BaseListFragment.java
@@ -1,13 +1,68 @@
 package com.gofar.component.basiclib;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.gofar.component.basiclib.entity.BaseListResponse;
+import com.gofar.component.basiclib.list.ListLoader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+
+import ezy.ui.layout.LoadingLayout;
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import me.yokeyword.fragmentation.SupportFragment;
 
 /**
  * @author lcf
  * @date 2018/6/7 17:06
  * @since 1.0
  */
-public class BaseListFragment extends Fragment{
+public abstract class BaseListFragment<T> extends SupportFragment {
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
+    private RefreshLayout mRefreshLayout;
+    private LoadingLayout mLoadingLayout;
+    private RecyclerView mRecyclerView;
+
+    private ListLoader<T> mListLoader;
+    private BaseQuickAdapter<T, ? extends BaseViewHolder> mAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.base_list, container, false);
+        mRefreshLayout = view.findViewById(R.id.refreshLayout);
+        mLoadingLayout = view.findViewById(R.id.loadingLayout);
+        mRecyclerView = view.findViewById(R.id.recyclerView);
+        initView();
+        return view;
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        mListLoader.load(getObservable(0));
+    }
+
+    private void initView() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mAdapter);
+        mListLoader = new ListLoader<T>(mLoadingLayout, mRefreshLayout, mAdapter, mCompositeDisposable) {
+            @Override
+            protected Observable<BaseListResponse<T>> getObservable(int pageIndex) {
+                return null;
+            }
+        };
+    }
+
+    protected abstract Observable<BaseListResponse<T>> getObservable(int pageIndex);
 
 }
